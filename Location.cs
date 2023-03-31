f9d4e6825c9ce077a1b3b1d125f87808048ddc1e
Captain <89099266+CaptainAbdullah@users.noreply.github.com>
2023-03-31 23:02:32 +0500

    Add files via upload

---
+++ Location.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MYRIDE
+{
+    internal class Location
+    {
+        float latitude;
+        float longitude;
+
+        public void setLocation(float longitude,float latitude)
+        {
+            this.longitude = longitude;
+            this.latitude = latitude;
+        }
+        public void setLocation(string loc)
+        {
+            string[] words = loc.Split(',');
+            latitude = float.Parse(words[0]);
+            longitude = float.Parse(words[1]);
+        }
+
+
+        public float getLatitude() { return latitude; }
+        public float getLongitude() { return longitude; }
+    }
+}
